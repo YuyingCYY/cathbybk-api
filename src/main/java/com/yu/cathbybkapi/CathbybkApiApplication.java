d9cfdb380c57f267529be44de943322c57c31dff
YuyingCYY <120092424+YuyingCYY@users.noreply.github.com>
2025-01-09 17:39:04 +0800

    [FIX] 運行時檢查是否已經有資料

--- src/main/java/com/yu/cathbybkapi/CathbybkApiApplication.java
+++ src/main/java/com/yu/cathbybkapi/CathbybkApiApplication.java
@@ -1,13 +1,32 @@
 package com.yu.cathbybkapi;
 
+import com.yu.cathbybkapi.repository.PriceRepository;
+import com.yu.cathbybkapi.repository.ProductRepository;
+import com.yu.cathbybkapi.service.ApiService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CathbybkApiApplication {
+public class CathbybkApiApplication implements CommandLineRunner {
+
+  private final ProductRepository productRepository;
+  private final ApiService apiService;
+
+  public CathbybkApiApplication(
+    ProductRepository productRepository, ApiService apiService) {
+    this.productRepository = productRepository;
+    this.apiService = apiService;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(CathbybkApiApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    if (productRepository.count() == 0) {
+      apiService.fetchData();
+    }
+  }
 }
